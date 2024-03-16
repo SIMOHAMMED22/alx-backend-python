47deeb0b5f4a092acc2e32242e8ca6b59f31c454
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-16 00:11:00 +0000

    type

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,13 +3,17 @@
 This module contains a function that returns a list with each
 element of the input list repeated a number of times.
 """
+from typing import Tuple
 
-from typing import List, Tuple, Union
-
-
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    zoomed_in: List = [
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
+    zoomed_in: Tuple[int, ...] = tuple([
         item for item in lst
         for i in range(factor)
-    ]
+    ])
     return zoomed_in
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
