2f5da73a7716660e2ad83924013cc4d750c0001d
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-16 00:11:44 +0000

    type

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,12 +5,14 @@ element of the input list repeated a number of times.
 """
 from typing import Tuple
 
+
 def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
     zoomed_in: Tuple[int, ...] = tuple([
         item for item in lst
         for i in range(factor)
     ])
     return zoomed_in
+
 
 array = (12, 72, 91)
 
