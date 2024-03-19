4495e7d09ed85636a804e7e997e24fd5d32239df
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-19 00:08:48 +0000

    runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_runtime(n, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     """
     Calculate the average time taken to run the wait_n function
                                         with the given parameters.
